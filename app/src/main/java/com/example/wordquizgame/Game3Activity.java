98bda0b7ef640fbfff7c336705d0d616e7d00ea4
wichit <chitchyza@gmail.com>
2016-04-05 15:13:38 +0700

    update

--- app/src/main/java/com/example/wordquizgame/Game3Activity.java
+++ app/src/main/java/com/example/wordquizgame/Game3Activity.java
@@ -1,78 +1,147 @@
 package com.example.wordquizgame;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class Game3Activity extends ActionBarActivity {
 
-    //
+    //Explicit
     private ImageView imageView;
     private TextView textView;
     private LinearLayout linearLayout;
-    private int[] qestionImageInts;
-    private  String[] answerStrings;
+    private int[] questionImageInts;
+    private String[] answerStrings, chooseStrings;
+    private String showTextString = "";
+    private int timesAnInt = 0, countAnInt = 0;
+    private StringBuilder stringBuilder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game3);
 
+        //Bind Widget
         imageView = (ImageView) findViewById(R.id.imageView);
         textView = (TextView) findViewById(R.id.textView3);
-
         linearLayout = (LinearLayout) findViewById(R.id.linButton);
 
-        MyDatalmage myDatalmage = new MyDatalmage();
-        qestionImageInts = myDatalmage.questionImageInts;
-        answerStrings = myDatalmage.answerStrings;
+        //Get Data
+        MyDatalmage myDataImage = new MyDatalmage();
+        questionImageInts = myDataImage.questionImageInts;
+        answerStrings = myDataImage.answerStrings;
+        chooseStrings = myDataImage.chooseStrings;
+        stringBuilder = new StringBuilder();
 
+        //Clear Answer
+        clearAnswer();
+
+        //Show Question Image
         showQuestionImage(0);
 
-        clearAnswer();
-        
+
+        //Create Button
         createButton(0);
-    }//Main method
+
+    }   // Main Method
 
     private void createButton(int intIndex) {
 
-        char[] answerChars = answerStrings[intIndex].toCharArray();
+        final char[] answerChars = chooseStrings[intIndex].toCharArray();
 
-
-
-
-        ViewGroup.LayoutParams layoutParams = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+        LinearLayout.LayoutParams layoutParams = new LinearLayout
+                .LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
                 ViewGroup.LayoutParams.WRAP_CONTENT);
 
-        for (int i=0;i< answerChars.length;i++){
-            Button button = new Button(this);
-            button.setId(i + 1);
+
+        for (int i = 0; i < answerChars.length; i++) {
+
+            final Button button = new Button(this);
+            button.setId(i);
             button.setText(String.valueOf(answerChars[i]));
+
+            button.setLayoutParams(layoutParams);
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    Log.d("5April", "You Click Label = " + answerChars[view.getId()]);
+                    addTextToTextView(String.valueOf(answerChars[view.getId()]));
+
+                    button.setVisibility(View.INVISIBLE);
+
+
+                    timesAnInt += 1;
+                    if (timesAnInt >= answerChars.length) {
+                        checkAnswer();
+                    }
+
+
+                }   // onClick
+            });
+
 
             linearLayout.addView(button);
 
+        }   //for
 
+    }   // createButton
+
+    private void checkAnswer() {
+
+        if (answerStrings[countAnInt].equals(textView.getText().toString())) {
+            countAnInt += 1;
         }
 
+        clearAnswer();
+        showQuestionImage(countAnInt);
+        createButton(countAnInt);
 
+    }   // checkAnswer
+
+    private void addTextToTextView(String strAdd) {
+
+        Log.d("5April", "ค่่าที่ได้ strAdd ==> " + strAdd);
+
+        stringBuilder.append(strAdd);
+
+        Log.d("5April", "stringBuild ==> ที่สระสมได้ " + stringBuilder.toString());
+
+        showTextString = stringBuilder.toString();
+
+        Log.d("5April",
+                "่ค่าของ showTextString ที่รับได้ ==> " + showTextString);
+
+        forSetText(showTextString);
+
+
+    }   // addText
+
+    private void forSetText(String showTextString) {
+        textView.setText(showTextString);
     }
 
     private void clearAnswer() {
 
+        timesAnInt = 0;
         textView.setText("");
 
+        stringBuilder.setLength(0);
+        Log.d("5April", "stringBuild ตอน Clear ==> " + stringBuilder.toString());
+        linearLayout.removeAllViews();
 
-    }
+    }   // clearAnswer
 
     private void showQuestionImage(int intIndex) {
 
-        imageView.setImageResource(qestionImageInts[intIndex]);
-    }
+        imageView.setImageResource(questionImageInts[intIndex]);
 
+    }   // showQuestion
 
-} // Main Class
+}   // Main Class
